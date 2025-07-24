bc67e1326de7eb1c4d9284524c44f0fd4d09ca49
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-07-23 19:39:44 -0700

    Updates app/init to connect to db

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,34 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from sqlalchemy import create_engine
 from flask import Flask
 from flask_cors import CORS
 from .db import db, migrate
 import os
 
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+
 def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+    engine = create_engine(DATABASE_URL)
+
+    try:
+        with engine.connect() as connection:
+            print("Connection successful!")
+    except Exception as e:
+        print(f"Failed to connect: {e}\n\n\n")
+        raise Exception(f"Database connection failed: {e}")
 
     if config:
         app.config.update(config)
