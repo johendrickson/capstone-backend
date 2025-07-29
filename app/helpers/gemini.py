5ca31b871952e62343622d7ebb4752125d42a4f1
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-07-29 17:53:27 -0500

    init Gemini client w/ vertexai

--- app/helpers/gemini.py
+++ app/helpers/gemini.py
@@ -4,7 +4,10 @@ from google import genai
 
 # Initialize the Gemini client using the GEMINI_API_KEY environment variable
 client = genai.Client(
-    # api_key=os.getenv("GEMINI_API_KEY")
+    api_key=os.getenv("GEMINI_API_KEY"),
+    vertexai=True
+    # project=os.getenv("GOOGLE_CLOUD_PROJECT"),
+    # location=os.getenv("GOOGLE_CLOUD_LOCATION")
 )
 
 def generate_plant_info_from_scientific_name(scientific_name):
